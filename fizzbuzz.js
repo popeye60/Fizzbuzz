d772da9e16b3ff3de3eb7bb7b6cf3498b36711cc
popeye60 <Gaidmanee@gmail.com>
2019-09-21 15:36:15 +0700

    commit jest

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,16 @@
-for (number = 1; number < 1000; number++) {
-       if (number % 3 === 0) {
-         return "***";
-       }else if (number % 7 == 0){
-       return number  
-        }else if (number % 3 == 0 && number %7 == 0){
-            return
-        }
+class FizzBuzz {
+  static say(number) {
+    return 1;
+  }
+}
+module.exports = FizzBuzz;
+
+// for (number = 1; number < 1000; number++) {
+//   if (number % 3 === 0) {
+//     console.log("fizz");
+//   } else if (number % 7 == 0) {
+//     console.log("fizz");
+//   } else if (number % 3 == 0 && number % 7 == 0) {
+//     console.log("fizz");
+//   } else console.log("buzz");
+// }
