878da8316ae8d354de7b32276c73aaef470f8c78
popeye60 <Gaidmanee@gmail.com>
2019-09-21 15:13:08 +0700

    ass jest

---
+++ fizzbuzz.js
@@ -0,0 +1,8 @@
+for (number = 1; number < 1000; number++) {
+       if (number % 3 === 0) {
+         return "***";
+       }else if (number % 7 == 0){
+       return number  
+        }else if (number % 3 == 0 && number %7 == 0){
+            return
+        }
