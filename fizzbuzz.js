e8e676232235a3de741a217d7e20b86a4035b6f4
popeye60 <Gaidmanee@gmail.com>
2019-09-21 16:17:36 +0700

    edit jest

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,23 +1,25 @@
 class FizzBuzz {
   static say(number) {
-    if (number.toEqul % 3 === 0) {
-      return "fizz";
-    } else if (number.toEqul % 5 === 0) {
-      return "buzz";
-    } else if (number.toEqul % 3 === 0 && number.toEqul % 5 === 0) {
-      return "fizzbuzz";
-    } else return number;
+    switch (true) {
+      case number % 15 === 0:
+        return "fizzbuzz";
+      case number % 5 === 0:
+        return "buzz";
+      case number % 3 === 0:
+        return "fizz";
+      default:
+        return number;
+    }
   }
 }
-
 module.exports = FizzBuzz;
 
-// for (number = 1; number < 1000; number++) {
 //   if (number % 3 === 0) {
-//     console.log("fizz");
-//   } else if (number % 7 == 0) {
-//     console.log("fizz");
-//   } else if (number % 3 == 0 && number % 7 == 0) {
-//     console.log("fizz");
-//   } else console.log("buzz");
+//     return "fizz";
+//   } else if (number % 5 === 0) {
+//     return "fizz";
+//   } else if (number % 3 === 0 && number % 5 === 0) {
+//     return "fizzbuzz";
+//   } else
+//      return number;
 // }
