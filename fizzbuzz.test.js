909a260e44132a93fa434dfed4308012764bffd9
popeye60 <Gaidmanee@gmail.com>
2019-09-22 11:10:09 +0700

    Add

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -5,15 +5,15 @@ describe("Test FizzBuzz", () => {
     const result = FizzBuzz.say(1);
     expect(result).toEqual(1); //Assert Function
   });
-  test("Test 3 expect 3", () => {
+  test("Test 3 expect 2", () => {
     const result = FizzBuzz.say(3);
     expect(result).toString("fizz"); //Assert Function
   });
-  test("Test 5 expect 5", () => {
+  test("Test 5 expect 3", () => {
     const result = FizzBuzz.say(5);
     expect(result).toString("buzz"); //Assert Function
   });
-  test("Test 15 expect 15", () => {
+  test("Test 15 expect 4", () => {
     const result = FizzBuzz.say(15);
     expect(result).toString("fizzbuzz"); //Assert Function
   });
